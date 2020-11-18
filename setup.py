088ce8eede304a824a9e2c1766bf34edfc605c4c
Greyson Christoforo <grey@christoforo.net>
2020-11-18 21:57:01 +0000

    not otter

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="otter_saver",
+    name="mqtt_saver",
     use_scm_version=True,
     setup_requires=["setuptools_scm"],
     author="James Ball",
@@ -12,7 +12,7 @@ setuptools.setup(
     description="Save live data",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/jmball/otter_saver",
+    url="https://github.com/jmball/mqtt_saver",
     py_modules=["saver"],
     classifiers=[
         "Programming Language :: Python :: 3",
