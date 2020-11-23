5e3cbdf94847479f372b89f87308ba1555b95f37
Greyson Christoforo <grey@christoforo.net>
2020-11-23 17:40:24 +0000

    PEP517 compliant packaging

--- setup.py
+++ setup.py
@@ -1,22 +1,4 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
 
-setuptools.setup(
-    name="mqtt_saver",
-    use_scm_version=True,
-    setup_requires=["setuptools_scm"],
-    author="James Ball",
-    author_email="",
-    description="Save live data",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/jmball/mqtt_saver",
-    py_modules=["saver"],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPL-3.0",
-        "Operating System :: OS Independent",
-    ],
-)
+setup()
